fbc1d0f3a391d27acabc69141f6a82ad1bf958e1
Artur Prepelytsia <preart81@gmail.com>
2025-09-15 00:08:12 +0300

    update var syntax in: - modules/jenkins/jobs/goit_django_docker.groovy - modules/jenkins/values.yaml -

--- modules/jenkins/jobs/goit_django_docker.groovy
+++ modules/jenkins/jobs/goit_django_docker.groovy
@@ -1,16 +1,17 @@
-pipelineJob("goit-django-docker") {
+pipelineJob('goit-django-docker') {
   definition {
     cpsScm {
       scm {
         git {
           remote {
-            url(${github_repo_url})
-            credentials("github-token")
+            url('${github_repo_url}')
+            credentials('github-token')
           }
-          branches("*/lesson-7")
+          // branches('*/lesson-7')
+          branches('*/${github_branch}')
         }
       }
-      scriptPath("Jenkinsfile")
+      scriptPath('Jenkinsfile')
     }
   }
 }
